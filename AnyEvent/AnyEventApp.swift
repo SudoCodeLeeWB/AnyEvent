877ae1a9ca74001598422ced2f12dec6c2e92926
LeeWonBeen /  이원빈 <84519146+SudoCodeLeeWB@users.noreply.github.com>
2024-01-24 22:01:55 +0900

    Initial Commit

---
+++ AnyEvent/AnyEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyEventApp.swift
+//  AnyEvent
+//
+//  Created by 이원빈 on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AnyEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
